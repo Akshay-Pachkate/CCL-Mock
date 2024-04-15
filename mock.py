6ff13caccb92eb128438abcf03e43a44dd037fa7
Akshay Pachkate <141637488+Akshay-Pachkate@users.noreply.github.com>
2024-04-15 15:35:29 +0530

    Create mock.py

---
+++ mock.py
@@ -0,0 +1,6 @@
+name = input("Enter name: ")
+rollno = input("Enter roll No: ")
+dept = input("Enter Department: ");
+
+for i in range(5):
+  print("Name: ", name, " Roll Number: ", rollno, " Department: ", dept)
